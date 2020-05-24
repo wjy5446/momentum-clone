9a3f0ca341a9f475f44600a356fbb86fecae7afc
wjy5446 <wjy5446@gmail.com>
2020-05-24 22:19:12 +0900

    feat) update clock automatly

--- js/clock.js
+++ js/clock.js
@@ -7,11 +7,14 @@ function getTime() {
   const minutes = date.getMinutes();
   const hours = date.getHours();
 
-  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
   getTime();
+  setInterval(getTime, 1000);
 }
 
 init();
