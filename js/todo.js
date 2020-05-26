7d3ba30104f11202c13adf501a5c810123cc8366
wjy5446 <wjy5446@gmail.com>
2020-05-26 23:54:42 +0900

    feat) store todo

--- js/todo.js
+++ js/todo.js
@@ -3,19 +3,32 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
 
 function paintToDo(text) {
   console.log(text);
   const li = document.createElement("li");
-
   const span = document.createElement("span");
-  span.innerText = text;
   const delBtn = document.createElement("button");
+  const newId = toDos.length + 1;
+
+  span.innerText = text;
   delBtn.innerText = "x";
 
   li.appendChild(span);
   li.appendChild(delBtn);
+  li.id = newId;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId + 1,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handleSubmit(event) {
@@ -25,8 +38,12 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function (toDo) {
+      paintToDo(toDo.text);
+    });
   }
 }
 
