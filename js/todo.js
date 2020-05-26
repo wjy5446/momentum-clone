882a4d9dceafd6bb817a24b45919e873e2c4c9ab
wjy5446 <wjy5446@gmail.com>
2020-05-27 00:16:55 +0900

    feat) delete todo

--- js/todo.js
+++ js/todo.js
@@ -3,14 +3,25 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
-const toDos = [];
+let toDos = [];
+
+function deleteToDos(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
 function paintToDo(text) {
-  console.log(text);
   const li = document.createElement("li");
   const span = document.createElement("span");
   const delBtn = document.createElement("button");
@@ -19,13 +30,15 @@ function paintToDo(text) {
   span.innerText = text;
   delBtn.innerText = "x";
 
+  delBtn.addEventListener("click", deleteToDos);
+
   li.appendChild(span);
   li.appendChild(delBtn);
   li.id = newId;
   toDoList.appendChild(li);
   const toDoObj = {
     text: text,
-    id: newId + 1,
+    id: newId,
   };
   toDos.push(toDoObj);
   saveToDos();
