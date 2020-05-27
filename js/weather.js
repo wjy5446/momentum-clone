9ce0b0e911220008aece167f913598beba7dc06c
wjy5446 <wjy5446@gmail.com>
2020-05-27 23:06:32 +0900

    feat) add geo data

---
+++ js/weather.js
@@ -0,0 +1,38 @@
+const API_KEY = "72a5e5f820cce3978271001fc5c5f5fc";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  console.log(position);
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("cant access!!");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  }
+}
+
+function init() {
+  loadCoords();
+}
+init();
