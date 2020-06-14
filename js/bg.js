55596000eb4dace10020b0e521a164d3002b0e9c
wjy5446 <wjy5446@gmail.com>
2020-06-14 15:25:11 +0900

    fix) does not show images

--- js/bg.js
+++ js/bg.js
@@ -6,7 +6,7 @@ function handleLaod() {
 }
 
 function paintImage(imgNumber) {
-  const pathImage = `img/${imgNumber + 1}.jpg`;
+  const pathImage = `momumtum-clone/img/${imgNumber + 1}.jpg`;
   character.style.backgroundImage = `url("${pathImage}")`;
 }
 
